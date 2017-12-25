1931982924da77114d02f757b2fe95c64abc4d5e
tjjazzy14 <trm5073@gmail.com>
2017-12-25 04:09:15 -0500

    Initial, Created Database Project

---
+++ Controllers/GalleryController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ImageSPA.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageSPA.Controllers
+{
+    public class GalleryController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new GalleryIndexModel();
+            return View();
+        }
+    }
+}
