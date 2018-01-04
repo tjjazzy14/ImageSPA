c23abcca059af4b5b4983fe30005ada95e23741f
tjjazzy14 <trm5073@gmail.com>
2018-01-04 12:15:46 -0500

    Created styles and detail functions

---
+++ Controllers/ImageController.cs
@@ -0,0 +1,19 @@
+﻿using ImageSPA.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageSPA.Controllers
+{
+    public class ImageController : Controller
+    {
+        public IActionResult Upload()
+        {
+            var model = new UploadImageModel();
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult UploadNewImage()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Models/UploadImageModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ImageSPA.Models
+{
+    public class UploadImageModel
+    {
+        public string Title { get; set; }
+        public string Tags { get; set; }
+        public IFormFile ImageUpload { get; set; }
+    }
+}
