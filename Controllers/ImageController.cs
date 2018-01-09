9fb2a28070e043da9a0af08fdf2753cdea22312a
tjjazzy14 <trm5073@gmail.com>
2018-01-09 13:11:47 -0500

    Configure Azure Blob Storage and connected the blob to the App

--- Controllers/ImageController.cs
+++ Controllers/ImageController.cs
@@ -1,19 +1,48 @@
 ﻿using ImageSPA.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SimpleImageGallery.Data;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace ImageSPA.Controllers
 {
     public class ImageController : Controller
     {
+        private IConfiguration _config;
+        private IImage _imageService;
+        private string AzureConnectionString { get; }
+
+        //Constructor to get the dependency
+        public ImageController(IConfiguration config, IImage imageService)
+        {
+            _config = config;
+            _imageService = imageService;
+            AzureConnectionString = _config["AzureStorageConnectionString"];
+        }
+
         public IActionResult Upload()
         {
+            
             var model = new UploadImageModel();
             return View(model);
         }
         [HttpPost]
-        public IActionResult UploadNewImage()
+        public async Task<IActionResult> UploadNewImage(IFormFile file, string tags, string title)
         {
-            return Ok();
+            var container = _imageService.GetBlobContainer(AzureConnectionString, "spaimages");
+            var content = ContentDispositionHeaderValue.Parse(file.ContentDisposition);
+            var fileName = content.FileName.Trim('"');
+             
+            //Get a Reference to a Block Blob
+            var blockBlob = container.GetBlockBlobReference(fileName);
+            await blockBlob.UploadFromStreamAsync(file.OpenReadStream());
+
+            await _imageService.SetImage(title, tags, blockBlob.Uri);
+
+            return RedirectToAction("Index", "Gallery");
+            
         }
     }
 }
